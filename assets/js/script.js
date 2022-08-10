4631a69995b8626fd2fdcd25114f692746cb8910
Boluwatife-AJB <56480003+Boluwatife-AJB@users.noreply.github.com>
2022-08-10 02:01:19 +0100

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,12 +15,17 @@ accordionItems.forEach((item) => {
 
 const toggleItem = (item) => {
   const accordionContent = item.querySelector('.questions__content');
+  const accordionIcon = item.querySelector('.questions__icon');
 
   if (item.classList.contains('accordion-open')) {
     accordionContent.removeAttribute('style');
     item.classList.remove('accordion-open');
+    accordionIcon.style.transform = 'rotate(450deg)';
+    accordionIcon.style.transition = '.3s';
+    // console.log(accordionIcon);
   } else {
     accordionContent.style.height = accordionContent.scrollHeight + 'px';
     item.classList.add('accordion-open');
+    accordionIcon.style.transform = 'rotate(270deg)';
   }
 };
